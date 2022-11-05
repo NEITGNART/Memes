f4ac06fd9de97b9d3540373a6ec53d6208c9a734
Pham Tien <19127576@student.hcmus.edu.vn>
2022-11-05 16:44:45 +0700

    Deploy on netlify

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from "styled-components";
-import {LazyLoadImage} from "react-lazy-load-image-component";
-import {Blurhash} from "react-blurhash";
+
 import CustomImage from "./CustomImage";
 
 type Images = {
